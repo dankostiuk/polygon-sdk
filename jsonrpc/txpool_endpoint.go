c6052eb5dd1d1b9f2c1fd5bea75c5293c122da5d
dankostiuk <dank216@gmail.com>
2021-08-05 10:52:54 -0400

    test out removing V, R, S

--- jsonrpc/txpool_endpoint.go
+++ jsonrpc/txpool_endpoint.go
@@ -12,18 +12,18 @@ type Txpool struct {
 }
 
 type ContentResponse struct {
-	Pending   map[types.Address]map[uint64]*gethPendingTransaction    `json:"pending"`
-	Queued    map[types.Address]map[uint64]*gethQueuedTransaction     `json:"queued"`
+	Pending map[types.Address]map[uint64]*gethPendingTransaction `json:"pending"`
+	Queued  map[types.Address]map[uint64]*gethQueuedTransaction  `json:"queued"`
 }
 
 type InspectResponse struct {
-	Pending   map[string]map[string]string  `json:"pending"`
-	Queued		map[string]map[string]string  `json:"queued"`
+	Pending map[string]map[string]string `json:"pending"`
+	Queued  map[string]map[string]string `json:"queued"`
 }
 
 type StatusResponse struct {
-	Pending   uint64	`json:"pending"`
-	Queued 		uint64	`json:"queued"`
+	Pending uint64 `json:"pending"`
+	Queued  uint64 `json:"queued"`
 }
 
 type gethPendingTransaction struct {
@@ -33,9 +33,6 @@ type gethPendingTransaction struct {
 	To          *types.Address `json:"to"`
 	Value       argBig         `json:"value"`
 	Input       argBytes       `json:"input"`
-	V           argByte        `json:"v"`
-	R           argBytes       `json:"r"`
-	S           argBytes       `json:"s"`
 	Hash        types.Hash     `json:"hash"`
 	From        types.Address  `json:"from"`
 	BlockHash   interface{}    `json:"blockHash"`
@@ -50,9 +47,6 @@ type gethQueuedTransaction struct {
 	To          *types.Address `json:"to"`
 	Value       argBig         `json:"value"`
 	Input       argBytes       `json:"input"`
-	V           argByte        `json:"v"`
-	R           argBytes       `json:"r"`
-	S           argBytes       `json:"s"`
 	Hash        types.Hash     `json:"hash"`
 	From        types.Address  `json:"from"`
 	BlockHash   types.Hash     `json:"blockHash"`
@@ -61,12 +55,7 @@ type gethQueuedTransaction struct {
 }
 
 func toGethPendingTransaction(t *types.Transaction, b *types.Block) *gethPendingTransaction {
-	if t.R == nil {
-		t.R = []byte{0}
-	}
-	if t.S == nil {
-		t.S = []byte{0}
-	}
+
 	return &gethPendingTransaction{
 		Nonce:       argUint64(t.Nonce),
 		GasPrice:    argBig(*t.GasPrice),
@@ -74,24 +63,16 @@ func toGethPendingTransaction(t *types.Transaction, b *types.Block) *gethPending
 		To:          t.To,
 		Value:       argBig(*t.Value),
 		Input:       argBytes(t.Input),
-		V:           argByte(t.V), 
-		R:           argBytes(t.R),
-		S:           argBytes(t.S),
 		Hash:        t.Hash,
 		From:        t.From,
-		BlockHash: 	 b.Hash(),
+		BlockHash:   b.Hash(),
 		BlockNumber: nil,
-		TxIndex: 		 nil,
+		TxIndex:     nil,
 	}
 }
 
 func toGethQueuedTransaction(t *types.Transaction, b *types.Block, txIndex int) *gethQueuedTransaction {
-	if t.R == nil {
-		t.R = []byte{0}
-	}
-	if t.S == nil {
-		t.S = []byte{0}
-	}
+
 	return &gethQueuedTransaction{
 		Nonce:       argUint64(t.Nonce),
 		GasPrice:    argBig(*t.GasPrice),
@@ -99,9 +80,6 @@ func toGethQueuedTransaction(t *types.Transaction, b *types.Block, txIndex int) 
 		To:          t.To,
 		Value:       argBig(*t.Value),
 		Input:       argBytes(t.Input),
-		V:           argByte(t.V),
-		R:           argBytes(t.R), // needs to be 0x0
-		S:           argBytes(t.S), // needs to be 0x0
 		Hash:        t.Hash,
 		From:        t.From,
 		BlockHash:   b.Hash(),
@@ -110,7 +88,7 @@ func toGethQueuedTransaction(t *types.Transaction, b *types.Block, txIndex int) 
 	}
 }
 
-/** 
+/**
  * Content - implemented according to https://geth.ethereum.org/docs/rpc/ns-txpool#txpool_content
  */
 func (t *Txpool) Content() (interface{}, error) {
@@ -125,14 +103,14 @@ func (t *Txpool) Content() (interface{}, error) {
 			if block == nil {
 				block = &types.Block{
 					Header: &types.Header{
-						Hash: blockHash,
+						Hash:   blockHash,
 						Number: uint64(0),
 					},
 				}
 			}
 			pendingRpcTxns[address][nonce] = toGethPendingTransaction(tx, block)
 		}
-  }
+	}
 	queuedRpcTxns := make(map[types.Address]map[uint64]*gethQueuedTransaction)
 	for address, nonces := range queuedTxs {
 		queuedRpcTxns[address] = make(map[uint64]*gethQueuedTransaction)
@@ -143,7 +121,7 @@ func (t *Txpool) Content() (interface{}, error) {
 			if block == nil {
 				block = &types.Block{
 					Header: &types.Header{
-						Hash: blockHash,
+						Hash:   blockHash,
 						Number: uint64(0),
 					},
 				}
@@ -151,17 +129,17 @@ func (t *Txpool) Content() (interface{}, error) {
 			// using 0 as txIndex
 			queuedRpcTxns[address][nonce] = toGethQueuedTransaction(tx, block, 0)
 		}
-  }
+	}
 
 	resp := ContentResponse{
 		Pending: pendingRpcTxns,
-		Queued: queuedRpcTxns,
+		Queued:  queuedRpcTxns,
 	}
-	
+
 	return resp, nil
 }
 
-/** 
+/**
  * Inspect - implemented according to https://geth.ethereum.org/docs/rpc/ns-txpool#txpool_inspect
  */
 func (t *Txpool) Inspect() (interface{}, error) {
@@ -174,7 +152,7 @@ func (t *Txpool) Inspect() (interface{}, error) {
 			msg := fmt.Sprintf("%d wei + %d gas x %d wei", tx.Value, tx.Gas, tx.GasPrice)
 			pendingRpcTxns[address.String()][fmt.Sprint(nonce)] = msg
 		}
-  }
+	}
 
 	queuedRpcTxns := make(map[string]map[string]string)
 	for address, nonces := range queuedTxs {
@@ -183,33 +161,33 @@ func (t *Txpool) Inspect() (interface{}, error) {
 			msg := fmt.Sprintf("%d wei + %d gas x %d wei", tx.Value, tx.Gas, tx.GasPrice)
 			queuedRpcTxns[address.String()][fmt.Sprint(nonce)] = msg
 		}
-  }
+	}
 
 	resp := InspectResponse{
 		Pending: pendingRpcTxns,
-		Queued: queuedRpcTxns,
+		Queued:  queuedRpcTxns,
 	}
 
 	return resp, nil
 }
 
-/** 
+/**
  * Status - implemented according to https://geth.ethereum.org/docs/rpc/ns-txpool#txpool_content
  */
 func (t *Txpool) Status() (interface{}, error) {
 	pendingTxs, queuedTxs := t.d.store.GetTxs()
 	var pendingCount int
-  for _, t := range pendingTxs {
-    pendingCount += len(t)
-  }
+	for _, t := range pendingTxs {
+		pendingCount += len(t)
+	}
 	var queuedCount int
-  for _, t := range queuedTxs {
-    queuedCount += len(t)
-  }
+	for _, t := range queuedTxs {
+		queuedCount += len(t)
+	}
 
 	resp := StatusResponse{
 		Pending: uint64(pendingCount),
-		Queued: uint64(queuedCount),
+		Queued:  uint64(queuedCount),
 	}
 
 	return resp, nil
